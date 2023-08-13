d0ad6ffa51e29363ce49039b8d4dc7c9851a2fa1
adv6273 <90032570+adv6273@users.noreply.github.com>
2023-08-13 10:58:15 +0530

    added ntlify link to log in and signup pages

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -10,7 +10,7 @@ const Signup = (props) => {
     e.preventDefault();
     const {name,email,password} =credentials;
     try {
-      const response = await fetch("http://localhost:5000/api/auth/createuser", {
+      const response = await fetch("https://zesty-chaja-b0fb67.netlify.app//api/auth/createuser", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
